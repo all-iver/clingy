f2adcb031b0a9e05de9a2f3d72570bbb7505348e
Oliver Smith <oliver@sub-c.org>
2018-04-08 22:57:08 -0700

    More examples

---
+++ Clingy/Examples/Scripts/ClingyExamples05.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SubC.Attachments;
+
+public class ClingyExamples05 : MonoBehaviour {
+
+    public Attacher attacher;
+    public GameObject anchorPosBtn, anchorNegBtn;
+    bool toggle;
+
+	// Use this for initialization
+	void Start () {
+        OnPosClicked();
+	}
+
+    public void OnPosClicked() {
+        anchorPosBtn.SetActive(false);
+        anchorNegBtn.SetActive(true);
+        attacher.attachment.objects.Get(0, 0).defaultParams.SetParam(new Param(new Vector2(-0.5f, 0.5f), "anchor"));
+    }
+
+    public void OnNegClicked() {
+        anchorPosBtn.SetActive(true);
+        anchorNegBtn.SetActive(false);
+        attacher.attachment.objects.Get(0, 0).defaultParams.SetParam(new Param(new Vector2(-0.5f, -0.5f), "anchor"));
+    }
+
+}
