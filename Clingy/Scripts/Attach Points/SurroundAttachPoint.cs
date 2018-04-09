d34ee6a8fe9f6473bc5648859179c6c8e44f5597
Oliver Smith <oliver@sub-c.org>
2018-04-09 01:52:16 -0700

    More work on examples

--- Clingy/Scripts/Attach Points/SurroundAttachPoint.cs
+++ Clingy/Scripts/Attach Points/SurroundAttachPoint.cs
@@ -7,13 +7,22 @@ namespace SubC.Attachments {
 
         public string outputPosition = "surroundPos";
         public float distance;
+        public bool animate;
+        public float animationSpeed = 90;
+        float offset;
 
         public override void ApplyParamsForOther(AttachObject other, AttachObject self) {
             int count = self.attachment.objects.Count(other.category);
-            float degree = (360 / count) * other.indexInCategory;
+            float degree = (360 / count) * other.indexInCategory + offset;
             float radians = degree * Mathf.Deg2Rad;
             Vector3 pos = new Vector3(Mathf.Cos(radians), Mathf.Sin(radians)) * distance;
             other.resolvedParams.SetParam(new Param(pos, outputPosition));
+        }
+
+        void Update() {
+            if (!animate)
+                return;
+            offset += Time.deltaTime * animationSpeed;
         }
 
 	}
